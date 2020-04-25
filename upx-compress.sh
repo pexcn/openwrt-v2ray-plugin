47daa07a9e4d135a5f22318ff126651df898f320
pexcn <i@pexcn.me>
2020-04-25 20:15:24 +0800

    use upx default compression algorithm
    close #2

--- upx-compress.sh
+++ upx-compress.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 set -o pipefail
 
-upx --lzma bin/arm/v2ray-plugin_linux_arm7
-upx --lzma bin/aarch64/v2ray-plugin_linux_arm64
-upx --lzma --best bin/mips/v2ray-plugin_linux_mips_sf
-upx --lzma --best bin/mipsel/v2ray-plugin_linux_mipsle_sf
+upx bin/arm/v2ray-plugin_linux_arm7
+upx bin/aarch64/v2ray-plugin_linux_arm64
+upx --best bin/mips/v2ray-plugin_linux_mips_sf
+upx --best bin/mipsel/v2ray-plugin_linux_mipsle_sf
